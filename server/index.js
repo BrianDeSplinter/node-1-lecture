67a233cfdadbc98a5c3de3491cee8d163f071b98
BrianDeSplinter <bdesplinter34@gmail.com>
2020-05-19 11:51:37 -0600

    Added Restful properties

--- server/index.js
+++ server/index.js
@@ -7,11 +7,19 @@ const SERVER_PORT = 4000
 
 app.use(express.json())
 
+app.use(function(req,res,next){
+    console.log('We got a request yo')
+    next()
+})
+
 app.get('/api/brian', (req, res) =>{
     res.status(200).send('Brian')
 })
 
 app.get('/api/users', ctrl.getAllUsers)
 app.get('/api/users/:user_id', ctrl.getUserById)
+app.post('/api/users', ctrl.createUser)
+app.put('/api/users/:user_id', ctrl.editUser)
+app.delete('/app/users/:user_id', ctrl.deleteUser)
 
 app.listen(SERVER_PORT, () => console.log(`Server running on port ${SERVER_PORT}`)) 
